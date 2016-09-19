19b5a2b81c32e30930fcbe02a6668521924be483
Thomas W Rackers <twrackers@gmail.com>
2016-09-18 22:07:57 -0400

    Adjusted indentation.

--- OneShot/src/OneShot.h
+++ OneShot/src/OneShot.h
@@ -5,28 +5,28 @@
 
 class OneShot : public StateMachine
 {
-  private:
-    const unsigned int m_delay;   // length in msec of on-state after trigger
-    unsigned long m_timeout;      // time of next update
-    bool m_triggered;             // triggered state
+private:
+  const unsigned int m_delay;   // length in msec of on-state after trigger
+  unsigned long m_timeout;      // time of next update
+  bool m_triggered;             // triggered state
     
-  public:
-    // Constructor
-    // @param dly delay in msec between trigger and state returning to off
-    OneShot(const unsigned int dly);
-    
-    // Check if it's time to update this object's state.
-    // @return true if update of state occurred, false otherwise
-    virtual bool update();
-    
-    // Trigger this object, setting it to on state and setting time when
-    // it will turn off again, assuming it doesn't get retriggered first.
-    void trigger();
-    
-    // Returns true if this object has been triggered but has not timed out yet;
-    // false otherwise.
-    // @return triggered state.
-    bool isTriggered() const;    
+public:
+  // Constructor
+  // @param dly delay in msec between trigger and state returning to off
+  OneShot(const unsigned int dly);
+  
+  // Check if it's time to update this object's state.
+  // @return true if update of state occurred, false otherwise
+  virtual bool update();
+  
+  // Trigger this object, setting it to on state and setting time when
+  // it will turn off again, assuming it doesn't get retriggered first.
+  void trigger();
+  
+  // Returns true if this object has been triggered but has not timed out yet;
+  // false otherwise.
+  // @return triggered state.
+  bool isTriggered() const;    
 };
 
 #endif
